7e49aa806d1861b88b7b7a2eba1b3ee3e31e99f0
Felicitas <felicitas.aguerralde@gmail.com>
2023-08-24 15:05:00 -0300

    Agregue los 4 puntos

--- src/Rectangulo/main.java
+++ src/Rectangulo/main.java
@@ -2,17 +2,16 @@ package Rectangulo;
 
 public class main {
     public static void main (String args[]){
+        //Creo los puntos
+        Punto p1 = new Punto(3,0);
+        Punto p2 = new Punto(6,0);
+        Punto p3 = new Punto(3,4);
+        Punto p4 = new Punto(6,4);
 
         //Creo los objetos
         //Rectangulo
-        Rectangulo rectangulo = new Rectangulo();
-        //Creo los puntos
-        Punto p1 = new Punto();
-        Punto p2 = new Punto();
+        Rectangulo rectangulo = new Rectangulo(p1,p2,p3,p4);
 
-        //Seteo los puntos
-        p1.setPunto(0,2);
-        p2.setPunto(0,6);
 
         //Imprimo el rectangulo
         System.out.println("El rectángulo es:"+rectangulo.getRectangulo());
